7b4c8b2059a8ca5e39c29ea37500a35862458d02
Nihal Shukla <nihalgdev002@gmail.com>
2021-11-02 16:08:59 +0530

    final app

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGO_URI: "mongodb+srv://testing:test123@cluster0.psut4.mongodb.net/testing?retryWrites=true&w=majority"
+    MONGO_URI: "mongodb+srv://yourname:yourpassword@cluster0.psut4.mongodb.net/yourdatabasename?retryWrites=true&w=majority"
 }
